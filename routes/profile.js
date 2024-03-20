a3d150469b317770dee924e56052bfeb13a7545b
Eduardo rodriguez <eduardo.rq.590@gmail.com>
2024-03-20 19:29:25 -0400

    add post method and it's tests

--- routes/profile.js
+++ routes/profile.js
@@ -1,32 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+const userProfileController = require("../controllers/userAccount.controller");
 const router = express.Router();
 
 const profiles = [
   {
-    "id": 1,
-    "name": "A Martinez",
-    "description": "Adolph Larrue Martinez III.",
-    "mbti": "ISFJ",
-    "enneagram": "9w3",
-    "variant": "sp/so",
-    "tritype": 725,
-    "socionics": "SEE",
-    "sloan": "RCOEN",
-    "psyche": "FEVL",
-    "image": "https://soulverse.boo.world/images/1.png",
-  }
+    id: 1,
+    name: "A Martinez",
+    description: "Adolph Larrue Martinez III.",
+    mbti: "ISFJ",
+    enneagram: "9w3",
+    variant: "sp/so",
+    tritype: 725,
+    socionics: "SEE",
+    sloan: "RCOEN",
+    psyche: "FEVL",
+    image: "https://soulverse.boo.world/images/1.png",
+  },
 ];
 
-module.exports = function() {
-
-  router.get('/*', function(req, res, next) {
-    console.log('asd')
-    res.render('profile_template', {
+module.exports = function () {
+  router.get("/*", function (req, res, next) {
+    res.render("profile_template", {
       profile: profiles[0],
     });
   });
 
+  router.post("/", (req, res) => {
+    userProfileController.addUser(req, res);
+  });
+
   return router;
-}
+};
--- tests/profile.spec.js
+++ tests/profile.spec.js
@@ -1,11 +1,19 @@
 const request = require("supertest");
-const { describe, it, afterAll } = require('@jest/globals');
+const { describe, it, afterAll } = require("@jest/globals");
 const { app } = require("../app");
+const newUser = require("./mocks/mockedUser.mock.json");
 
 describe("API / - conversations", () => {
-    it("Should get mocked profile", async () => {
-        const req = await request(app).get('/').expect(200);
-		expect(req.type).toBe('text/html');
-        expect(req.text).toBeDefined();
-	});
-})
+  it("Should get mocked profile", async () => {
+    const req = await request(app).get("/").expect(200);
+    expect(req.type).toBe("text/html");
+    expect(req.text).toBeDefined();
+  });
+  it("Should create an user profile", async () => {
+    const req = await request(app).post("/").send({...newUser}).expect(200);
+    expect(req.type).toBe("text/html");
+    expect(req.text).toBeDefined();
+    expect(req.text.includes('Kawasaki')).toBe(true);
+    expect(req.text.includes(newUser.description)).toBe(true);
+  });
+});
